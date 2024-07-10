31dde19f7b402a48ad57ed62a1ef5c9a96e96053
Talha Islam <86923018+CrazyTalha@users.noreply.github.com>
2024-07-10 05:59:53 +0500

    Added event-listeners to each gridBox to change its color when the mouse enters the gridBox.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector('.container');
+const amtOfGrids = 16;
+
 
 const grid = (amtOfGrids) => {
     for (i=0; i < amtOfGrids; i++) {
@@ -6,14 +8,25 @@ const grid = (amtOfGrids) => {
         row.classList.add('grid-row');
 
         for (j=0; j < amtOfGrids; j++) {
+            const widthAndHeight = 960 / amtOfGrids;
             const gridBox = document.createElement('div');
             gridBox.classList.add('grid-box');
 
+            gridBox.style.width = `${widthAndHeight}px`;
+            gridBox.style.height = `${widthAndHeight}px`;
+
             row.appendChild(gridBox);
+
+            gridBox.addEventListener('mouseover', () => {
+                gridBox.style.backgroundColor = 'red';
+            })
         }
 
         container.appendChild(row);
     }
+    
 }
 
-grid(16);
+
+
+grid(amtOfGrids);
